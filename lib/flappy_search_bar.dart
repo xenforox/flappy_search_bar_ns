f747732983a6564efce7adf9cd317b6980065d68
Mehdig95 <61311449+xenforox@users.noreply.github.com>
2025-05-08 17:33:32 +0100

    Update flappy_search_bar.dart
    changed library name

--- lib/flappy_search_bar.dart
+++ lib/flappy_search_bar.dart
@@ -3,7 +3,7 @@ library flappy_search_bar;
 import 'dart:async';
 
 import 'package:async/async.dart';
-import 'package:flappy_search_bar/scaled_tile.dart';
+import 'package:flappy_search_bar_ns/scaled_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
